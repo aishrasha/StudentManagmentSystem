85bac833e7272a8003d28ad27d21375b618190c3
aishrasha <153727243+aishrasha@users.noreply.github.com>
2023-12-27 11:30:32 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,3 @@
-
 <!--<p>welcome user</p>-->
 
 <!DOCTYPE html>
@@ -7,23 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-</head>
-<body>
-<?php
-        include 'loginNavbar.php';
-        require 'db.php';<!--<p>welcome user</p>-->
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
 <?php
         //include 'loginNavbar.php';
-        //require 'db.php';
+        require 'db.php';
         ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
@@ -43,69 +31,58 @@
         </li>
       </ul>
     </div>
+    </div>
+    <div class="topnav-right">
+    <a  class="text-info" href="#" style="text-decoration: none;"><i class="fa-solid fa-user"></i>Profile</a>&nsbp;
+    <a class="text-info" href="#"  style="text-decoration: none;"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
+  </div>
   </nav>
         <div class="container">
             <h1 class="mt-3 text-center">Users of Student CRUD System.</h1>
-
-            <table class="table table-striped mt-3">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Yashvi Ratuad</td>
-      <td>Yashvi@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Shivani sharma</td>
-      <td>Shivani@gmail.com</td>
-      <td>user</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Palak sharma</td>
-      <td>palak@gmail.com</td>
-      <td>user</td>
-    </tr>
-
-    <tr>
-      <th scope="row">4</th>
-      <td>Ayush sharma</td>
-      <td>Ayush@gmail.com</td>
-      <td>user</td>
-    </tr>
-
-    <tr>
-      <th scope="row">5</th>
-      <td>Shivangi soni</td>
-      <td>shivangi@gmail.com</td>
-      <td>user</td>
-    </tr>
-
-    <tr>
-      <th scope="row">6</th>
-      <td>Muskan sharma</td>
-      <td>muskan@gmail.com</td>
-      <td>user</td>
-    </tr>
-
-    <tr>
-      <th scope="row">7</th>
-      <td>Abhishek patidar</td>
-      <td>abhishek@gmail.com</td>
-      <td>user</td>
-    </tr>
-  </tbody>
-</table>
+          </div> 
+          <br><br>
+        <div class="container">
+            <?php
+                $sql = "SELECT * from users ORDER by id ASC;";
+                $res = mysqli_query($con,$sql);
+                if($res){
+                    if(mysqli_num_rows($res)>0){
+                        echo '<table class="table table-bordered table-striped">';
+                        echo "<thead>";
+                            echo "<tr>";
+                                echo "<th>ID</th>";
+                                echo "<th>Email</th>";
+                                echo "<th>Role</th>";
+                            echo "</tr>";
+                        echo "</thead>";
+                        echo "<tbody>";
+                        while($row = mysqli_fetch_array($res)){
+                            echo "<tr>";
+						        echo "<td>" . $row['id'] . "</td>";
+						        echo "<td>" . $row['email'] . "</td>";
+						        //echo "<td>" . $row['role'] . "</td>";
+						    echo "<tr>";
+                        }
+                    echo "</tbody>";                            
+                    echo "</table>";
+                    mysqli_free_result($res);
+                    }
+                    else{
+                        echo '<div class="alert alert-danger">
+                                No Record were Found.
+                            </div>';
+                    }
+                }
+                else{
+                    echo 'OOPs! something went wrong. please try again later.';
+                 }
+            mysqli_close($con);
+            ?>
         </div>
-    
+       
 </body>
 </html>
+<?php
+//include 'db.php';
+ //echo "<script>alert('welcome user');</script> ";
+?>

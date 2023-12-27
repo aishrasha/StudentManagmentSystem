70fbb615c72b7c6d5cbbdda90e6378262de631d0
aishrasha <153727243+aishrasha@users.noreply.github.com>
2023-12-27 13:05:24 +0530

    Update manage.php

--- manage.php
+++ manage.php
@@ -9,7 +9,7 @@
 <body>
 <?php
         //include 'loginNavbar.php';
-        //require 'db.php';
+        require 'db.php';
         ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
@@ -37,90 +37,59 @@
   <div class="container">
             <h1 class="mt-3 text-center">Users of Student CRUD System.</h1>
 
-            <table class="table table-striped mt-3">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-      <th scope="col">View</th>
-      <th scope="col">Edit</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Yashvi Ratuad</td>
-      <td>Yashvi@gmail.com</td>
-      <td>user</td>
-     <td><i class="fa-solid fa-eye"></i></td>
-     <td><i class="fa-solid fa-pen-to-square"></i></td>
-     <td><i class="fa-solid fa-trash"></i></td>
-     
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Shivani sharma</td>
-      <td>Shivani@gmail.com</td>
-      <td>user</td>
-      <td><i class="fa-solid fa-eye"></i></td>
-      <td><i class="fa-solid fa-pen-to-square"></i></td>
-      <td><i class="fa-solid fa-trash"></i></td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Palak sharma</td>
-      <td>palak@gmail.com</td>
-      <td>user</td>
-      <td><i class="fa-solid fa-eye"></i></td>
-      <td><i class="fa-solid fa-pen-to-square"></i></td>
-      <td><i class="fa-solid fa-trash"></i></td>
-    </tr>
-
-    <tr>
-      <th scope="row">4</th>
-      <td>Ayush sharma</td>
-      <td>Ayush@gmail.com</td>
-      <td>user</td>
-      <td><i class="fa-solid fa-eye"></i></td>
-      <td><i class="fa-solid fa-pen-to-square"></i></td>
-      <td><i class="fa-solid fa-trash"></i></td>
-    </tr>
-
-    <tr>
-      <th scope="row">5</th>
-      <td>Shivangi soni</td>
-      <td>shivangi@gmail.com</td>
-      <td>user</td>
-      <td><i class="fa-solid fa-eye"></i></td>
-     <td><i class="fa-solid fa-pen-to-square"></i></td>
-     <td><i class="fa-solid fa-trash"></i></td>
-    </tr>
-
-    <tr>
-      <th scope="row">6</th>
-      <td>Muskan sharma</td>
-      <td>muskan@gmail.com</td>
-      <td>user</td>
-      <td><i class="fa-solid fa-eye"></i></td>
-      <td><i class="fa-solid fa-pen-to-square"></i></td>
-      <td><i class="fa-solid fa-trash"></i></td>
-    </tr>
-
-    <tr>
-      <th scope="row">7</th>
-      <td>Abhishek patidar</td>
-      <td>abhishek@gmail.com</td>
-      <td>user</td>
-      <td><i class="fa-solid fa-eye"></i></td>
-      <td><i class="fa-solid fa-pen-to-square"></i></td>
-      <td><i class="fa-solid fa-trash"></i></td>
-    </tr>
-  </tbody>
-</table>
         </div>
+        <br><br>
+        <div class="container">
+            <?php
+                $sql = "SELECT * from users ORDER by id ASC;";
+                $res = mysqli_query($con,$sql);
+                if($res){
+                    if(mysqli_num_rows($res)>0){
+                        echo '<table class="table table-bordered table-striped">';
+                        echo "<thead>";
+                            echo "<tr>";
+                                echo "<th>ID</th>";
+                                echo "<th>Email</th>";
+                                echo "<th>Role</th>";
+                                echo "<th>View</th>";
+                                echo "<th>Edit</th>";
+                                echo "<th>Delete</th>";
+                            echo "</tr>";
+                        echo "</thead>";
+                        echo "<tbody>";
+                        while($row = mysqli_fetch_array($res)){
+                            echo "<tr>";
+						        echo "<td>" . $row['id'] . "</td>";
+						        echo "<td>" . $row['email'] . "</td>";
+						        echo "<td>" . 'user' . "</td>";
+                    echo "<td>";
+        						echo '<a href="read.php?id='. $row['id'] .'" class="mr-3 btn btn-light" title="View Details" data-toggle="tooltip"><span class="fa-regular fa-eye"></span></a></td>';
+                    echo "<td>";
+                    echo '<a href="update.php?id='. $row['id'] .'" class="mr-3 btn btn-light" title="Update Details" data-toggle="tooltip"><span class="fa-regular fa-pen-to-square"></span></a></td>';
+                    echo "<td>";
+                    echo '<a href="javascript:void(0)" title="Change Status" class="delete_btn_ajax btn btn-light ms-1" data-toggle="tooltip"><span class="fa-solid fa-trash"></span></a>';
+					        	echo '<input type="hidden" class="delete_id_value" value='.$row["id"].'>';
+                                echo '</td>';
+                  
+						        echo "<tr>";
+                        }
+                    echo "</tbody>";                            
+                    echo "</table>";
+                    mysqli_free_result($res);
+                    }
+                    else{
+                        echo '<div class="alert alert-danger">
+                                No Record were Found.
+                            </div>';
+                    }
+                }
+                else{
+                    echo 'OOPs! something went wrong. please try again later.';
+                 }
+            mysqli_close($con);
+            ?>
+        </div>
+       
     
 </body>
 </html>
